0311901eb92a3626287894773c08918269fd8110
priyanka <priyankamahajan369@gmail.com>
2021-11-19 10:20:59 -0500

    added searchbar.

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -18,13 +18,16 @@ export default function Header(props) {
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/about">About</Link>
-              </li>
-              
-            </ul>
-            <form className="d-flex">
+              </li>  
+                    </ul>
+
+              {props.searchBar?
+              <form className="d-flex">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            </form>:""}
+
+
           </div>
         </div>
       </nav>
